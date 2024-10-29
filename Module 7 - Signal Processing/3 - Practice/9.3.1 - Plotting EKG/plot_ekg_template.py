2d114758335cfca87a9eff4e5c21128842370b63
bloomgt <bloomgt@dukes.jmu.edu>
2024-10-29 11:49:20 -0400

    commit 10/29

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -3,16 +3,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # import the CSV file using numpy
-path = '../../../data/ekg/mitdb_201.csv'
+path = "../../../data/ekg/mitdb_201.csv"
 
 # load data in matrix from CSV file; skip first two rows
 
-### Your code here ###
+data = np.genfromtxt(path, delimiter=',')
 
 # save each vector as own variable
 
-### Your code here ###
+time =  data[2:,0]
+mlii = data [2:,1]
+voltage = data[2:,2]
 
 # use matplot lib to generate a single
 
-### Your code here ###
+plt.plot(time,voltage)
+plt.xlabel("Time [s]")
+plt.ylabel("Voltage [V]")
+plt.show()
--- Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
@@ -19,7 +19,11 @@ Step #1: load data in matrix from CSV file; skip first two rows. Call the data s
 """
 
 signal = 0
-## YOUR CODE HERE ##
+data = np.genfromtxt(signal_filepath, delimiter=',')
+
+time =  data[2:,0]
+mlii = data [2:,1]
+voltage = data[2:,2]
 
 """
 Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). These may not be correctly in radians
@@ -30,21 +34,25 @@ Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). The
 """
 Step 3: Pass data through weighted differentiator
 """
-
-## YOUR CODE HERE ##
-
+diff_conv = [1,-1]
+diffs = np.convolve(voltage,diff_conv)
 
 """
 Step 4: Square the results of the previous step
 """
  ## YOUR CODE HERE ##
 
+norm_data = np.square(diffs)
+
 """
 Step 5: Pass a moving filter over your data
 """
 
 ## YOUR CODE HERE
+window = [1,1,1,1,1,1,1,1,1,1]
+moving_average = np.convolve(voltage, window)
+
 # make a plot of the results. Can change the plot() parameter below to show different intermediate signals
 plt.title('Process Signal for ' + database_name)
-plt.plot(signal)
+plt.plot(time[len(norm_data)], norm_data)
 plt.show()
